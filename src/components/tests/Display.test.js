53868b8609981bb538ddd30c4723af18ef091ea0
sayo mizoguchi <sl.deve080412@gmail.com>
2021-09-03 14:04:19 -0700

    I finished MVP so hard

--- src/components/tests/Display.test.js
+++ src/components/tests/Display.test.js
@@ -1,17 +1,65 @@
+import React from "react";
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import Display from "./../Display";
+import Show from "./../Show";
 
+//we need to make a mock
+import fetchShow from "../../api/fetchShow";
+jest.mock("../../api/fetchShow");
 
+const testShow = {
+  name: "Sayo",
+  summary: "she need to go to Japan",
+  seasons: [
+    { id: 0, name: "season 1", episodes: [] },
+    { id: 1, name: "season 2", episodes: [] },
+  ],
+};
 
+test("that the Display component renders without any passed in props", () => {
+  render(<Display />);
+});
 
+test("renders show component when fetch bottun pressed", () => {
+  //Arrange
+  render(<Display show={testShow} />);
+  render(<Show show={testShow} selectedSeason={"none"} />);
+  fetchShow.mockResolvedValueOnce(testShow);
+  //Act
+  const button = screen.getByRole("button");
+  fireEvent.click(button);
 
+  //Assert
+  const select = screen.getByLabelText("Select A Season");
 
+  expect(select).toBeInTheDocument();
+});
 
+test("when fetch pressed, select options match amount of seasons ", () => {
+  render(<Display show={testShow} />);
+  render(<Show show={testShow} selectedSeason={"none"} />);
+  fetchShow.mockResolvedValueOnce(testShow);
+  //Act
+  const button = screen.getByRole("button");
+  fireEvent.click(button);
 
+  //Assert
+  const option = screen.getAllByTestId("season-option");
+  expect(option.length).toBe(testShow.seasons.length);
+});
 
+test("if displayFun fires after fetch is pressed", async () => {
+  fetchShow.mockResolvedValueOnce(testShow);
+  const displayFunc = jest.fn();
+  render(<Display show={testShow} displayFunc={displayFunc} />);
 
+  const button = screen.getByRole("button");
+  userEvent.click(button);
 
-
-
-
+  //Assert
+  await waitFor(() => expect(displayFunc).toHaveBeenCalled());
+});
 
 ///Tasks:
 //1. Add in nessisary imports and values to establish the testing suite.
